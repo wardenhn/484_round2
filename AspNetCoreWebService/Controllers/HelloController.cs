25ab2af09748af41875b6b00fc162507eb0c8468
AWS CodeStar <noreply-awscodestar@amazon.com>
2018-10-21 19:03:46 +0000

    Initial commit made by AWS CodeStar during project creation.

---
+++ AspNetCoreWebService/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebService.Controllers
+{
+    [Route("/")]
+    public class HelloController : Controller
+    {
+        private const string MESSAGE_FORMAT = "Hello {0}!";
+
+        [HttpGet]
+        public JsonResult Get([FromQuery] string name = "World")
+        {
+            return Json(new Response
+            {
+                output = string.Format(MESSAGE_FORMAT, name)
+            });
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody]string name = "World")
+        {
+            return Json(new Response
+            {
+                output = string.Format(MESSAGE_FORMAT, name)
+            });
+        }
+    }
+}
